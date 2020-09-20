be142c7ef04da1f03dbf1750f436996908ac9e3f
tunlynn <tunlynnag@gmail.com>
2020-09-20 17:48:22 +0630

    initial commit

---
+++ script.js
@@ -0,0 +1,70 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    if(!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+
+// Get Quote form API
+async function getQuote() {
+
+    showLoadingSpinner();
+
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // If Author is blank, add 'Unknown'
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown'
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+
+        // Reduce font size for long quote
+        if (data.quoteText.length > 120 ) {
+            quoteText.classList.add('long-quote')
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        
+        quoteText.innerText = data.quoteText;
+        
+        removeLoadingSpinner();
+
+        throw new Error ('oops!')
+    }
+    catch (error) {
+        console.log(error);
+        getQuote();
+    }
+}
+
+// Tweet Quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+getQuote();
